9d9b44f2c96c1785f565f48ac9912585a674ac4b
Rohit <motdharer1999@gmail.com>
2020-08-05 12:55:43 +0530

    Add files via upload

---
+++ HelloWorld/app.py
@@ -0,0 +1,26 @@
+print('Rohit Motdhare')
+print('o----')
+print(' ||||')
+print('*' * 10) # '*' * 10 is an expression
+
+# Variables-
+price = 10 # int value
+rating = 4.9 # float value
+name = 'Rohit' # string value
+is_published = True  # boolean value
+print(price)
+
+# We check in a patient named John Smith. He is 20 years old and is a new patient
+full_name = 'John Smith'
+age = 20
+is_new = True
+print(full_name, age, is_new)
+
+# Getting Input
+name = input('What is your name? ')
+print('Hi ' + name)
+
+# Ask 2 questions : person's name and favourite colour. Then, print a message like "Rohit likes blue"
+name = input('What is your name? ')
+color = input('What is your favourite colour? ')
+print(name + ' likes ' + color)
